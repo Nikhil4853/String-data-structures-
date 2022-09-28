64cfabc2c066eb206d629f1e742f96f65d55c15c
Nikhil4853 <nikhilkashyap.may021@gmail.com>
2022-09-28 06:57:51 +0530

    reverse palindrom and duplicate

---
+++ string_is_palindrom_or_not.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPalindrome(string s)
+
+{
+
+    int i = 0, j = s.length() - 1;
+
+    while (i < j)
+    {
+
+        if (s[i] != s[j])
+        {
+
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    cout << isPalindrome << endl;
+    return 0;
+}
