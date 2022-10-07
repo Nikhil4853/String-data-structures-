1eda2d772e31f05515933df7288aff5af0ead3bb
Nikhil4853 <nikhilkashyap.may021@gmail.com>
2022-10-07 23:35:18 +0530

    add question

--- reverseWordInAString.cpp
+++ reverseWordInAString.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+/*
+
+557. Reverse Words in a String III
+
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+*/
 string reverseWords(string s)
 {
     int i = 0;
